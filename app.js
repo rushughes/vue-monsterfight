ae76418bf70ffdfc97a438af3234fae4d7007777
Rus Hughes <russell.hughes@gmail.com>
2019-11-30 14:08:34 +0100

    https://www.udemy.com/course/vuejs-2-the-complete-guide/learn/lecture/5941834#overview

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ new Vue({
     playerHealth: 100,
     monsterHealth: 100,
     gameIsRunning: false,
+    turns: [],
   },
   methods: {
     startGame: function() {
@@ -12,8 +13,12 @@ new Vue({
       this.monsterHealth = 100;
     },
     attack: function() {
-      this.monsterHealth -= this.calculateDamage(3, 10);
-
+      var damage = this.calculateDamage(3, 10);
+      this.monsterHealth -= damage;
+      this.turns.unshift({
+        isPlayer: true,
+        text: 'Player hits Monster for ' + damage
+      });
       if (this.checkWin()) {
         return;
       }
@@ -40,7 +45,12 @@ new Vue({
       this.gameIsRunning = false;
     },
     monsterAttack: function () {
-      this.playerHealth -= this.calculateDamage(5, 12);
+      var damage = this.calculateDamage(5, 12);
+      this.playerHealth -= damage;
+      this.turns.unshift({
+        isPlayer: false,
+        text: 'Monster hits Player for ' + damage
+      });
       this.checkWin();
     },
     calculateDamage: function(min, max) {
